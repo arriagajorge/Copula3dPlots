5463ffe6c7c249a12f25e67b581304dd066a4e33
Jorge <jrg.poop@ciencias.unam.mx>
2023-05-04 10:26:46 -0600

    add Documentation

--- Plot3dCopulas.py
+++ Plot3dCopulas.py
@@ -1,13 +1,58 @@
+# import libraries and modify style
 import numpy as np
 import matplotlib.pyplot as plt
-
 plt.style.use('seaborn-v0_8-pastel')
-cmap = plt.get_cmap('coolwarm')
-plt.set_cmap(cmap)
-
+#cmap = plt.get_cmap('coolwarm')
+#plt.set_cmap(cmap)
 import statsmodels.distributions.copula.api as cp
 
-def CFD3d(cop, title, resl=50):
+def CFD3d(cop, title, resl=50, savefig=False, cmap='coolwarm', shr=0.5, pltshow=True, fontsizeTitle=16, fontsizeAxis=14,
+          xlabel='$u$', ylabel='$v$', zlabel='$C(u,v)$', figsize=(12,8), aspect=10):
+    """ Plot the CDF of a copula in 3D 
+    
+    The copula is evaluated in a grid of points (u,v) in [0,1]x[0,1]
+    
+    Parameters
+    ----------
+    cop : statsmodels.distributions.copula.api.Copula
+        Copula to plot.
+    title : str
+        Title of the plot.
+    resl : int, optional
+        Resolution of the plot, by default 50, which means 50x50 grid.
+    savefig : bool, optional
+        Save the figure, by default False.
+    cmap : str, optional
+        Color map to use, by default 'coolwarm'.
+    shr : float, optional
+        Shrink of the color bar, by default 0.5.
+    pltshow : bool, optional
+        Show the plot, by default True.
+    fontsizeTitle : int, optional
+        Font size of the title, by default 16.
+    fontsizeAxis : int, optional
+        Font size of the axis, by default 14.
+    xlabel : str, optional
+        Label of the x axis, by default '$u$'.
+    ylabel : str, optional
+        Label of the y axis, by default '$v$'.
+    zlabel : str, optional
+        Label of the z axis, by default '$C(u,v)$'.
+    figsize : tuple, optional
+        Size of the figure, by default (12,8).
+    aspect : int, optional
+        Aspect of the color bar, by default 10.
+    
+    Raises
+    ----------
+    NotImplementedError
+        CDF not available in closed form. If the copula to plot is the Student t copula.
+    
+    Returns
+    ----------
+    plt : matplotlib.pyplot
+        Plot of the CDF of the copula
+    """
     Z = np.zeros((resl, resl), dtype=np.float64)
     for i,u in enumerate(np.linspace(0,1, num=resl)):
         for j,v in enumerate(np.linspace(0,1, num=resl)):
@@ -17,21 +62,64 @@ def CFD3d(cop, title, resl=50):
     U, V = np.meshgrid(u, v)
 
     # Graficamos la copula de min(u,v) usando plot_surface() y decoramos el gráfico
-    fig = plt.figure(figsize=(12,8))
+    fig = plt.figure(figsize=figsize)
     ax = fig.add_subplot(111, projection='3d')
-    surf = ax.plot_surface(U, V, Z, cmap='coolwarm')
-    fig.colorbar(surf, shrink=0.5, aspect=10)
-    ax.set_xlabel('$u$', fontsize=14)
-    ax.set_ylabel('$v$', fontsize=14)
-    ax.set_zlabel('$C(u,v)$', fontsize=14)
+    surf = ax.plot_surface(U, V, Z, cmap=cmap)
+    fig.colorbar(surf, shrink=shr, aspect=aspect)
+    ax.set_xlabel(xlabel, fontsize=fontsizeAxis)
+    ax.set_ylabel(ylabel, fontsize=fontsizeAxis)
+    ax.set_zlabel(zlabel, fontsize=fontsizeAxis)
     #ax.set_zlim(0,10)
     #ax.view_init(elev=15, azim=90)
-    ax.set_title(title, fontsize=16)
-    plt.savefig(title + "cfd.pdf", format='pdf')
-    plt.show()
+    ax.set_title(title, fontsize=fontsizeTitle)
+    if savefig:
+        plt.savefig(title + "cfd.pdf", format='pdf')
+    if pltshow:
+        plt.show()
     return plt
 
-def PDF3d(cop, title, resl=50):
+def PDF3d(cop, title, resl=50, savefig=False, cmap='coolwarm', shr=0.5, pltshow=True, fontsizeTitle=16, fontsizeAxis=14,
+          xlabel='$u$', ylabel='$v$', zlabel='$c(u,v)$', figsize=(12,8), aspect=10):
+    """" Plot the PDF of a copula in 3D 
+    
+    The copula is evaluated in a grid of points (u,v) in [0,1]x[0,1]
+    
+    Parameters
+    ----------
+    cop : statsmodels.distributions.copula.api.Copula
+        Copula to plot.
+    title : str
+        Title of the plot.
+    resl : int, optional
+        Resolution of the plot, by default 50, which means 50x50 grid.
+    savefig : bool, optional
+        Save the figure, by default False.
+    cmap : str, optional
+        Color map to use, by default 'coolwarm'.
+    shr : float, optional
+        Shrink of the color bar, by default 0.5.
+    pltshow : bool, optional
+        Show the plot, by default True.
+    fontsizeTitle : int, optional
+        Font size of the title, by default 16.
+    fontsizeAxis : int, optional
+        Font size of the axis, by default 14.
+    xlabel : str, optional
+        Label of the x axis, by default '$u$'.
+    ylabel : str, optional
+        Label of the y axis, by default '$v$'.
+    zlabel : str, optional
+        Label of the z axis, by default '$c(u,v)$'.
+    figsize : tuple, optional
+        Size of the figure, by default (12,8).
+    aspect : int, optional
+        Aspect of the color bar, by default 10.
+    
+    Returns
+    ----------
+    plt : matplotlib.pyplot
+        Plot of the CDF of the copula
+    """
     Z = np.zeros((resl, resl), dtype=np.float64)
     for i,u in enumerate(np.linspace(0,1, num=resl)):
         for j,v in enumerate(np.linspace(0,1, num=resl)):
@@ -41,16 +129,18 @@ def PDF3d(cop, title, resl=50):
     U, V = np.meshgrid(u, v)
 
     # Graficamos la copula de min(u,v) usando plot_surface() y decoramos el gráfico
-    fig = plt.figure(figsize=(12,8))
+    fig = plt.figure(figsize=figsize)
     ax = fig.add_subplot(111, projection='3d')
-    surf = ax.plot_surface(U, V, Z, cmap='coolwarm')
-    fig.colorbar(surf, shrink=0.5, aspect=10)
-    ax.set_xlabel('$u$', fontsize=14)
-    ax.set_ylabel('$v$', fontsize=14)
-    ax.set_zlabel('$c(u,v)$', fontsize=14)
+    surf = ax.plot_surface(U, V, Z, cmap=cmap)
+    fig.colorbar(surf, shrink=shr, aspect=10)
+    ax.set_xlabel(xlabel, fontsize=fontsizeAxis)
+    ax.set_ylabel(ylabel, fontsize=fontsizeAxis)
+    ax.set_zlabel(zlabel, fontsize=fontsizeAxis)
     #ax.set_zlim(0,10)
     #ax.view_init(elev=15, azim=90)
-    ax.set_title(title, fontsize=16)
-    plt.savefig(title + "pdf.pdf", format='pdf')
-    plt.show()
+    ax.set_title(title, fontsize=fontsizeTitle)
+    if savefig:
+        plt.savefig(title + "pdf.pdf", format='pdf')
+    if pltshow:
+        plt.show()
     return plt
